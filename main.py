9265d5d1345605d65716c8272316bba7ad41bc2b
Joao-Victor23 <72052030+Joao-Victor23@users.noreply.github.com>
2021-03-22 21:44:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ def echo(update: Update, _: CallbackContext) -> None:
         update.message.reply_text(f'Tchau {update.effective_user.first_name} até logo!')
     if msg == "Sara":
         update.message.reply_text(f'Opa {update.effective_user.first_name}!, Precisa de mim?')
+    if msg == "sara":
+        update.message.reply_text(f'Opa {update.effective_user.first_name}!, precisa de mim?')
 def menu(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(">> LISTA DE COMANDOS  << \n /hello -> Exibe uma Mensagem para o usuario \n /news -> Mostra a Noticias mais Recentes \n /hacking_comands_android -> Mostra Comandos de Hacking Para Android")
 def hello(update: Update, context: CallbackContext) -> None:
