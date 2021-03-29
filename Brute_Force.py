e8d0ddae08e8ea17847f8f1942ebfc34ade55e5a
Joao-Victor23 <72052030+Joao-Victor23@users.noreply.github.com>
2021-03-29 13:41:41 -0300

    Update Brute_Force.py

--- Brute_Force.py
+++ Brute_Force.py
@@ -1,190 +1,27 @@
 import pyfiglet
-import random
-import time
 
-def two():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        password = a+b
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
-def three():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        c = random.choice(letras)
-        password = a+b+c
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
+figlet = pyfiglet.figlet_format("Brute Force")
+print(figlet)
 
-def for1():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        c = random.choice(letras)
-        d = random.choice(letras)
-        password = a+b+c+d
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
+print("Author    :  João Victor")
 
-def five():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        c = random.choice(letras)
-        d = random.choice(letras)
-        e = random.choice(letras)
-        password = a+b+c+d+e
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
+print("")
+print("")
 
-def six():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        c = random.choice(letras)
-        d = random.choice(letras)
-        e = random.choice(letras)
-        f = random.choice(letras)
-        password = a+b+c+d+e+f
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
-def seven():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        c = random.choice(letras)
-        d = random.choice(letras)
-        e = random.choice(letras)
-        f = random.choice(letras)
-        g = random.choice(letras)
-        password = a+b+c+d+e+f+g
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
+print("Digite uma senha com no maximo 3 sílabas!")
+senha = input(">>")
 
-def eigth():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        c = random.choice(letras)
-        d = random.choice(letras)
-        e = random.choice(letras)
-        f = random.choice(letras)
-        g = random.choice(letras)
-        h = random.choice(letras)
-        password = a+b+c+d+e+f+g+h
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
-
-def nine():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        c = random.choice(letras)
-        d = random.choice(letras)
-        e = random.choice(letras)
-        f = random.choice(letras)
-        g = random.choice(letras)
-        h = random.choice(letras)
-        i = random.choice(letras)
-        password = a+b+c+d+e+f+g+h+i
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
-
-def teen():
-    while True:
-        global combinacoes
-        a = random.choice(letras)
-        b = random.choice(letras)
-        c = random.choice(letras)
-        d = random.choice(letras)
-        e = random.choice(letras)
-        f = random.choice(letras)
-        g = random.choice(letras)
-        h = random.choice(letras)
-        i = random.choice(letras)
-        j = random.choice(letras)
-        password = a+b+c+d+e+f+g+h+i+j
-        print(password)
-        combinacoes +=1
-        if password == senha:
-            print("Senha encontrada")
-            print("foram feitas {} combinações".format(combinacoes))
-            break
-        
-
-        
-
-bt = pyfiglet.figlet_format("Brute Force")
-print("\033[1;32m"+bt)
-
-letras= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-numeros = [1,2,3,4,5,6,7,8,9,0]
+letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",'ba','be','bi','bo','bu','ca','ce','ci','co','cu','da','de','di','do','du','fa','fe','fi','fo','fu','ga','ge','gi','go','gu','ha','he','hi','ho','ia','ie','io','iu','hu','ja','je','ji','jo','ju','ka','ke','ki','ko','ku','la','le','li','lo','lu','ma','me','mi','mo','mu','na','ne','ni','no','nu','pa','pe','pi','po','pu','qu','ra','re','ri','ro','ru','sa','se','si','so','su','ta','te','ti','to','tu','va','ve','vi','vo','vu','wa','we','wi','wo','wu','xa','xe','xi','xo','xu','za','ze','zi','zo','zu', 'cha','che','chi','cho','chu','lha', 'lhe', 'lhi','lho', 'lhu', 'nha','nhe','nhi','nho','nhu','rra','rre','rri','rro','rru','ssa','sse','ssi','sso','ssu','qua','que','qui','quo']
 
 combinacoes = 0
 
-n = int(input("Quantidade de caracteres Minimo:2 Maximo:10 >>"))
-senha = str(input(">>"))
-
-
-
-if n == 2:
-    two()
-if n == 3:
-    three()
-if n == 4:
-    for1()
-if n == 5:
-    five()
-if n == 6:
-    six()
-if n == 7:
-    seven()
-if n == 8:
-    eigth()
-if n == 9:
-    nine()
-if n == 10:
-    teen()
+for i2 in letras:
+    for i3 in letras:
+        for i4 in letras:
+            password = (i2+i3+i4)
+            print(password)
+            combinacoes +=1
+            if password == senha:
+                print("Senha encontrada!")
+                print("Foram feitas um total de {} combinações até encontrar a senha!".format(combinacoes))
+                exit()
