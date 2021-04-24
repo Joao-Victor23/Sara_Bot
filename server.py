5798648cca7d9542442f71321f0d36240658f4d0
Joao-Victor23 <72052030+Joao-Victor23@users.noreply.github.com>
2021-04-24 06:49:34 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,46 +1,48 @@
 import socket
 import threading
+import pyfiglet
 
-HOST = input("Host: ")
-PORT = int(input("Port: "))
+B='\033[1;34m'
+C='\033[1;37m'
+CY='\033[1;36m'
+Y='\033[1;33m'
+G='\033[1;32m'
+RT='\033[;0m'
+R = "\033[1;31m"
+
+figlet = pyfiglet.figlet_format("Server",font="banner3-D")
+print(Y+figlet,C)
+print(f'{R}                                     [Version: 0.1]{C}')
+print(f"[{Y}i{C}] Recomendamos que Use portas Acima de 2000")
+HOST = input("[ Host ]: ")
+PORT = int(input("[ Port ]: "))
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((HOST,PORT))
 server.listen()
-print(f'Server is Up and Listening on {HOST}:{PORT}')
+print(f'O servidor está ativo  {HOST}:{PORT}')
 
 clients = []
 usernames = []
 
 def globalMessage(message):
     for client in clients:
-        client.send(message)
-
-def handleMessages(client):
-    while True:
-        try:
-            receiveMessageFromClient = client.recv(2048).decode('ascii')
-            globalMessage(f'{usernames[clients.index(client)]} :{receiveMessageFromClient}'.encode('ascii'))
-        except:
-            clientLeaved = clients.index(client)
-            client.close()
-            clients.remove(clients[clientLeaved])
-            clientLeavedUsername = usernames[clientLeaved]
-            print(f'{clientLeavedUsername} has left the chat...')
-            globalMessage(f'{clientLeavedUsername} has left us...'.encode('ascii'))
-            usernames.remove(clientLeavedUsername)
+        client.send(messageasernames[clientLeaved])
+        print(f'{clientLeavedUsername} saiu do chat...')
+        globalMessage(f'{clientLeavedUsername} deixou...'.encode('ascii'))
+        usernames.remove(clientLeavedUsername)
 
 
 def initialConnection():
     while True:
         try:
             client, address = server.accept()
-            print(f"New Connetion: {str(address)}")
+            print(f"Conexão nova: {str(address)}")
             clients.append(client)
             client.send('getUser'.encode('ascii'))
             username = client.recv(2048).decode('ascii')
             usernames.append(username)
-            globalMessage(f'{username} just joined the chat!'.encode('ascii'))
+            globalMessage(f'{username} entrou no chast!!'.encode('ascii'))
             user_thread = threading.Thread(target=handleMessages,args=(client,))
             user_thread.start()
         except:
