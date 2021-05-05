94fe0854a1ad46ec0225f5b449a5786f7ffb9361
Joao-Victor23 <72052030+Joao-Victor23@users.noreply.github.com>
2021-05-05 16:15:39 -0300

    Update Brute_Force02.py

--- Brute_Force02.py
+++ Brute_Force02.py
@@ -1,5 +1,6 @@
 import pyfiglet
 import os
+import time
 
 os.system("clear")
 
@@ -12,8 +13,19 @@ print("Author    :  João Victor")
 print("")
 print("")
 
-print("Digite uma senha com no maximo 3 sílabas!")
 senha = input(">>")
+
+print("[                    ] 0% ")
+time.sleep(2)
+print("[=====               ] 25%")
+time.sleep(2)
+print("[==========          ] 50%")
+time.sleep(2)
+print("[===============     ] 75%")
+time.sleep(2)
+print("[====================] 100%")
+time.sleep(2)
+
 
 letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",'ba','be','bi','bo','bu','ca','ce','ci','co','cu','da','de','di','do','du','fa','fe','fi','fo','fu','ga','ge','gi','go','gu','ha','he','hi','ho','ia','ie','io','iu','hu','ja','je','ji','jo','ju','ka','ke','ki','ko','ku','la','le','li','lo','lu','ma','me','mi','mo','mu','na','ne','ni','no','nu','pa','pe','pi','po','pu','qu','ra','re','ri','ro','ru','sa','se','si','so','su','ta','te','ti','to','tu','va','ve','vi','vo','vu','wa','we','wi','wo','wu','xa','xe','xi','xo','xu','za','ze','zi','zo','zu', 'cha','che','chi','cho','chu','lha', 'lhe', 'lhi','lho', 'lhu', 'nha','nhe','nhi','nho','nhu','rra','rre','rri','rro','rru','ssa','sse','ssi','sso','ssu','qua','que','qui','quo']
 
@@ -26,7 +38,4 @@ for i2 in letras:
                 password = i2+i3+i4+i5
                 combinacoes +=1
                 print(password)
-                if password == senha:
-                    print("Senha encontrada!")
-                    print("Foram feitas {} combinações".format(combinacoes))
-                    exit()
+print("Foram geradas cerca de {} senhas.".format(combinacoes))
