726be8d2e6c3f3435bc47d8dc8e6396a7145c41b
Joao-Victor23 <72052030+Joao-Victor23@users.noreply.github.com>
2021-05-05 21:16:19 -0300

    Update Brute_Force02.py

--- Brute_Force02.py
+++ Brute_Force02.py
@@ -27,15 +27,28 @@ print("[====================] 100%")
 time.sleep(2)
 
 
-letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",'ba','be','bi','bo','bu','ca','ce','ci','co','cu','da','de','di','do','du','fa','fe','fi','fo','fu','ga','ge','gi','go','gu','ha','he','hi','ho','ia','ie','io','iu','hu','ja','je','ji','jo','ju','ka','ke','ki','ko','ku','la','le','li','lo','lu','ma','me','mi','mo','mu','na','ne','ni','no','nu','pa','pe','pi','po','pu','qu','ra','re','ri','ro','ru','sa','se','si','so','su','ta','te','ti','to','tu','va','ve','vi','vo','vu','wa','we','wi','wo','wu','xa','xe','xi','xo','xu','za','ze','zi','zo','zu', 'cha','che','chi','cho','chu','lha', 'lhe', 'lhi','lho', 'lhu', 'nha','nhe','nhi','nho','nhu','rra','rre','rri','rro','rru','ssa','sse','ssi','sso','ssu','qua','que','qui','quo']
+letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 combinacoes = 0
 
-for i2 in letras:
-    for i3 in letras:
-        for i4 in letras:
-            for i5 in letras:
-                password = i2+i3+i4+i5
-                combinacoes +=1
-                print(password)
-print("Foram geradas cerca de {} senhas.".format(combinacoes))
+for i1 in letras:
+    for i2 in letras:
+        for i3 in letras:
+            for i4 in letras:
+                for i5 in letras:
+                	for i6 in letras:
+                		for i7 in letras:
+                			for i8 in letras:
+                				for i9 in letras:
+                					for i10 in letras:
+                						for i11 in letras:
+                							for i12 in letras:
+                								for i13 in letras:
+                									password = i1+i2+i3+i4+i5+i6+i7+i8+i9+i10+i11+i12+i13
+                									combinacoes += 1
+                									print(password)
+                									if password == senha:
+                										print('Sua Senha foi encontrada!')
+                										time.sleep(2)
+                										print('Foram geradas um total de {} senhas!'.format(password))
+                	
